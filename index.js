9ca5086dffc8b0bc2c7ad4090d75b4da78141688
William.Janssen <janssenwilliam02@gmail.com>
2024-06-07 11:54:21 +0200

    felhantering

--- index.js
+++ index.js
@@ -70,6 +70,11 @@ app.get('/posts', authenticate, async (req, res) => {
   res.json(posts);
 });
 
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({ error: 'Something went wrong!' });
+  });
+
 app.listen(3000, () => {
   console.log('Server is running on http://localhost:3000');
 });
